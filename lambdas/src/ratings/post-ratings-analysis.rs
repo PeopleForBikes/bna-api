12837d94cd029d9dcafbb210c948f19af8d9ad55
Rémy Greinhofer <remy.greinhofer@gmail.com>
2024-09-03 08:32:09 -0500

    Fix the POST ratings analysis endpoint (#142)
    Fixes the endpoint creating new rating analysis.
    
    Signed-off-by: Rémy Greinhofer <remy.greinhofer@gmail.com>

--- lambdas/src/ratings/post-ratings-analysis.rs
+++ lambdas/src/ratings/post-ratings-analysis.rs
@@ -46,3 +46,50 @@ async fn main() -> Result<(), Error> {
         e
     })
 }
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+    use aws_lambda_events::http;
+    use lambda_http::RequestExt;
+
+    #[tokio::test]
+    async fn test_handler_all() {
+        let event = http::Request::builder()
+            .header(http::header::CONTENT_TYPE, "application/json")
+            .body(Body::Text(
+                r#"{
+                  "cost": null,
+                  "end_time": null,
+                  "fargate_price_id": null,
+                  "fargate_task_arn": null,
+                  "result_posted": null,
+                  "s3_bucket": null,
+                  "sqs_message": null,
+                  "start_time": [
+                    2024,
+                    247,
+                    13,
+                    7,
+                    29,
+                    922293000,
+                    0,
+                    0,
+                    0
+                  ],
+                  "state_machine_id": "fc009967-c4d0-416b-baee-93708ac80cbc",
+                  "status": null,
+                  "step": "Analysis",
+                  "torn_down": null
+                }"#
+                .to_string(),
+            ))
+            .expect("failed to build request")
+            .with_request_context(lambda_http::request::RequestContext::ApiGatewayV2(
+                lambda_http::aws_lambda_events::apigw::ApiGatewayV2httpRequestContext::default(),
+            ));
+        let r = function_handler(event).await;
+        let _var_name = dbg!(r);
+    }
+}
